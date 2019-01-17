777838f01fb944572c28ec83630be8e5cf0cbcb7
Werner Kugler <werner.kugler@rubicon.eu>
2019-01-17 16:20:05 +0100

    RemoveOldFiles commented

--- Docs/RemoveOldFiles.cmd
+++ Docs/RemoveOldFiles.cmd
@@ -1,8 +1,12 @@
 REM %1 = days not touched
 set workdir=C:\temp\StpLog
 set mydir=Remover
+IF NOT EXIST %workdir%\%mydir% mkdir %workdir%\%mydir%
 set tempdir=%RANDOM%
 del %workdir%\%mydir%\Delete_*.txt
+REM Move
 ROBOCOPY %workdir% %workdir%\%tempdir% /S /MOV /MINLAD:%1 /XD %workdir%\%mydir% > %workdir%\%mydir%\Delete_%tempdir%.txt
-ROBOCOPY %workdir% %workdir% /S /MOVE /MINAGE:%1 /XD %workdir%\%mydir%
+REM Delete Files
+ROBOCOPY %workdir% %workdir% /S /MOVE /MINAGE:%1 /XD %workdir%\%mydir% 
+REM Delete remaining directories
 rmdir %workdir%\%tempdir% /S /q >> %workdir%\%mydir%\Delete_%tempdir%.txt
