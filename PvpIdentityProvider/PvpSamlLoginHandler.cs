f0ab7992c0619210f6425458b43c6b9878e29928
werner.kugler <werner.kugler@rubicon.eu>
2023-12-19 09:47:17 +0100

    Set AuthnContext according to SecClass

--- PvpIdentityProvider/PvpSamlLoginHandler.cs
+++ PvpIdentityProvider/PvpSamlLoginHandler.cs
@@ -10,6 +10,7 @@ using ComponentSpace.SAML2.Assertions;
 using ComponentSpace.SAML2.Configuration;
 using Egora.Pvp;
 using Egora.Stammportal.PvpIdentityProvider.AuthorizationWebService;
+using static System.Net.WebRequestMethods;
 
 namespace Egora.Stammportal.PvpIdentityProvider
 {
@@ -22,24 +23,44 @@ namespace Egora.Stammportal.PvpIdentityProvider
       string serviceProvider;
       SAMLIdentityProvider.ReceiveSSO(context.Request, out serviceProvider);
       var partner = SAMLController.Configuration.GetPartnerServiceProvider(serviceProvider);
-      var samlAttributes = GetSamlAttributes(partner, userName);
+      var samlAttributes = GetSamlAttributes(partner, userName, out var pvpVersion, out var secClass);
 
-      SAMLIdentityProvider.SendSSO(context.Response, userName, samlAttributes);
+      if (secClass >= 3)
+      {
+        // ToDo Check 2nd factor
+      }
+
+      string authnContext = pvpVersion.Contains("2.1")
+        ? secClass == 0 ? "http://www.ref.gv.at/ns/names/agiz/pvp/secclass/0" : $"http://www.ref.gv.at/ns/names/agiz/pvp/secclass/0-{secClass}"
+        : $"http://www.ref.gv.at/ns/names/agiz/pvp/secclass/{secClass}";
+      string assertionConsumerServiceUrl = partner.AssertionConsumerServiceUrl;
+      SAMLIdentityProvider.SendSSO(context.Response, userName, samlAttributes, authnContext, assertionConsumerServiceUrl);
     }
 
-    public static SAMLAttribute[] GetSamlAttributes(PartnerServiceProviderConfiguration partner, string userName)
+    public static SAMLAttribute[] GetSamlAttributes(PartnerServiceProviderConfiguration partner, string userName,
+      out string pvpVersion, out int secClass)
     {
       string rootUrl = partner.AssertionConsumerServiceUrl;
       var authorizer = new PvpAuthorizerSoapClient();
       Egora.Stammportal.PvpIdentityProvider.AuthorizationWebService.CustomAuthorization authorization = authorizer.GetAuthorization(rootUrl, userName);
-      if (authorization == null || authorization.Equals(Egora.Stammportal.CustomAuthorization.NoAuthorization))
-      {
+      if (authorization == null || authorization.HttpHeaders == null)
         throw new ApplicationException("No Authorization received.");
 
-      }
+      var noAuth = authorization.HttpHeaders[0];
+      var noAuthorizationHttpHeader = Egora.Stammportal.CustomAuthorization.NoAuthorization.HttpHeaders[0];
+      if (noAuth.Name == noAuthorizationHttpHeader.Name && noAuth.Value == noAuthorizationHttpHeader.Value)
+        throw new ApplicationException("NoAuthorization received.");
 
       if (!authorization.PvpVersion.Contains("2"))
         throw new ApplicationException($"Pvp Version 2.0 or 2.1 expected, but {authorization.PvpVersion} received");
+
+      pvpVersion = authorization.PvpVersion;
+
+      secClass = 0;
+      var pvpSecClass = authorization.HttpHeaders.Where(h => h.Name.Equals("X-PVP-SECCLASS", StringComparison.InvariantCultureIgnoreCase))
+        .Select(h => h.Value).Max();
+      if (pvpSecClass != null)
+        int.TryParse(pvpSecClass, out secClass);
 
       var attributes = authorization.SoapHeaderXmlFragment;
       var samlAttributes = attributes.Elements().Select(CreateSamlAttribute).ToArray();
